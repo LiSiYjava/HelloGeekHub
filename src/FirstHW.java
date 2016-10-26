5728a4a1d6ed686be8cc83095dbf8ed61c0d03f6
LiSiY <d:\Mail>
2016-10-26 14:35:20 +0300

    Initial commit

---
+++ src/FirstHW.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 26.10.2016.
+ */
+public class FirstHW {
+   public static void main(String[] args){
+       System.out.println("Hello GeekHub !!!");
+       System.out.print("I am happy ");
+       System.out.print("that I have got to GeekHub))");
+   }
+}
